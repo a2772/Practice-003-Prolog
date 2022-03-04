1d3074e4776e35c08469ef68ef105283966182a1
Paris Ramírez Saldaña <paris.140815@gmail.com>
2022-03-03 23:06:01 -0600

    Proyecto 2

--- Proyectos/14-02-DispositivosRotos.pl
+++ Proyectos/14-02-DispositivosRotos.pl
@@ -1,7 +1,7 @@
 /* Declarando el inicio */
-inicio :- !,nl,nl,nl,write('--->Averiguaremos dónde está la falla, según el caso<---'),nl,
+inicio :- nl,nl,nl,write('--->Averiguaremos dónde está la falla, según el caso<---'),nl,
           write('Sabemos que, la luz1, luz2 y calentador están conectados a fusible1'),nl,
-	  write('Además, luz3 y luz4 están conectadas a fusible2'),nl,
+          write('Además, luz3 y luz4 están conectadas a fusible2'),nl,
           write('Selecciona el dispositivo que quieres analizar:'),nl,
           write('0. Salir'),nl,
           write('1. Analizar luz1'),nl,
@@ -10,14 +10,14 @@ inicio :- !,nl,nl,nl,write('--->Averiguaremos dónde está la falla, según el c
           write('4. Analizar luz4'),nl,
           write('5. Analizar calentador'),nl,
           read(Opcion),
-	  ejecutar(Opcion).
+          ejecutar(Opcion).
 
-ejecutar(Opcion) :- 	Opcion == 1, anDispositivo(luz1), inicio;
-			Opcion == 2, anDispositivo(luz2), inicio;
-			Opcion == 3, anDispositivo(luz3), inicio;
-			Opcion == 4, anDispositivo(luz4), inicio;
-			Opcion == 5, anDispositivo(calentador), inicio;
-			Opcion == 0, true.
+ejecutar(Opcion) :-     Opcion == 1, anDispositivo(luz1), inicio;
+                        Opcion == 2, anDispositivo(luz2), inicio;
+                        Opcion == 3, anDispositivo(luz3), inicio;
+                        Opcion == 4, anDispositivo(luz4), inicio;
+                        Opcion == 5, anDispositivo(calentador), inicio;
+                        Opcion == 0, true.
 
 
 /* En caso de que quiera visualizar sin el menu cuales son dispositivos */
@@ -25,7 +25,7 @@ diferente(X,Y) :- \+ (X=Y).
 dispositivo(calentador).
 dispositivo(luz1).
 dispositivo(luz2).
-dispositivo(luz4).
+dispositivo(luz3).
 dispositivo(luz4).
 conectado(luz1,fusible1).
 conectado(luz2,fusible1).
@@ -34,34 +34,34 @@ conectado(luz3,fusible2).
 conectado(luz4,fusible2).
 
 anDispositivo(Dispositivo) :-   
-			      interruptorOn,
-		              fusibleOk(Dispositivo),
-		 	      \+ roto(Dispositivo),
-	   		      nl,write('El dispositivo funciona correctamente'),!.
+                              interruptorOn,
+                              fusibleOk(Dispositivo),
+                              \+ roto(Dispositivo),
+                              nl,write('El dispositivo funciona correctamente'),!.
 
 interruptorOn :- preguntar(el_interruptor_esta_encendido), !;
-		 write('Por favor encienda el interruptor'),nl,inicio.			     
+                 write('Por favor encienda el interruptor'),nl.
 
 fusibleOk(X) :- conectado(X,F),nl,nl,
-		write('Nota: El dispositivo '),
-		write(X),
-		write(' está conectado al fusible '),
-		write(F),nl,
-		preguntar(ese_fusible_esta_intacto), !;
-                write('Por favor reemplace el fusible '),inicio.
+                write('Nota: El dispositivo '),
+                write(X),
+                write(' está conectado al fusible '),
+                write(F),nl,
+                preguntar(ese_fusible_esta_intacto), !;
+                write('Por favor reemplace el fusible ').
 
 roto(Dispositivo) :-
-			nl,preguntar(ve_el_dispositivo_funcionar_en_este_momento),nl,
-			write('El dispositivo '),
-			write(Dispositivo),
-			write(' funciona correctamente'),!;
-			write('El problema es el dispositivo '),
-			write(Dispositivo),
-			write('. Debe repararlo o reemplazarlo'),inicio.
+                        nl,preguntar(ve_el_dispositivo_funcionar_en_este_momento),nl,
+                        write('El dispositivo '),
+                        write(Dispositivo),
+                        write(' funciona correctamente'),!;
+                        write('El problema es el dispositivo '),
+                        write(Dispositivo),
+                        write('. Debe repararlo o reemplazarlo').
 
 /* Default de la pregunta */
 hacepregunta(Preg) :-
-    write('Responda como si su vida dependiera de ello (si/no): ¿'),
+    write('Responda honestamente (si/no): ¿'),
     write(Preg),
     write('? '),
     read(Input),
